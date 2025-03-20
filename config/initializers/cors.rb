8d38a9d425654fed221995bf0286a6fd347ba611
Lucas Pizol Ferreira <lucaspizolfe@gmail.com>
2025-03-20 13:45:52 -0300

    feat: cors config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,7 +10,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     if Rails.env.development?
       origins 'localhost:3000', 'localhost:3001', 'https://my-app.com', 'localhost:5173'
     else
-      origins "https://software.rainbowpiercing.com.br/"
+      origins "https://software.rainbowpiercing.com.br"
     end
 
     resource "*",
